ac4a2f2b5a8f632a15d2da6207d296b14fb38405
Conor Sullivan <conorwsullivan@gmail.com>
2023-06-14 19:30:57 +0700

    Better git context info

--- bgit.py
+++ bgit.py
@@ -1,9 +1,9 @@
 from git import Repo
 import os
 
+import bui
+
 def info_source(config, role="system"):
-    # print('git_info_source')
-    # bgit.info_source('status,log:5,diff'),
     def git_info_source():
         try:
             repo = Repo(os.getcwd(), search_parent_directories=True)
@@ -15,16 +15,31 @@ def info_source(config, role="system"):
 
             for command in config.split(','):
                 if command == 'status':
-                    result.append(repo.git.status())
+                    status_output = repo.git.status()
+
+                    result.append(f"branch: {repo.active_branch}")
+                    if repo.is_dirty():
+                        result.append("dirty")
+                    if 'MERGING' in status_output:
+                        result.append("MERGE in progress")
+                    if 'REBASING' in status_output:
+                        result.append("REBASE in progress")
+
+                    lines = status_output.split('\n')
+                    modified_files = [line.replace('modified:', '').strip() for line in lines if 'modified:' in line]
+
+                    result.append(f"modified files: {', '.join(modified_files)}")
+
                 elif command == 'diff':
                     result.append("-- DIFF --\n" + repo.git.diff())
+
                 elif command.startswith('log:'):
                     result.append("-- HISTORY --\n" + repo.git.log('--oneline', '-' + command[4:]))
                 else:
                     pass
 
-            content = '\n\n'.join(result)
-            # print(content)
+            content = '\n'.join(result)
+            bui.print(content)
 
             return [{ "role": role, "content": content }]
 
